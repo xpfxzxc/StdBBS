580324f4ac8fb17f7b599209cc4e1d7fa54af6b8
xpfxzxc <xpfxzxc@gmail.com>
2019-10-05 16:36:01 +0800

    initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [],
+  exports: [NgbModule]
+})
+export class SharedModule {}
